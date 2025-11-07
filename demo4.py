988735fbd0f964979e9d4d551472bc9e1e12e7b3
chiru1812 <chirnthantchiranthant@gmail.com>
2025-11-07 09:31:40 +0530

    Add files via upload

---
+++ demo4.py
@@ -0,0 +1,19 @@
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
+
+# Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+
+# While with condition
+password = ""
+while password != "chiru":
+    password = input("Enter password: ")
+
+print("Access granted!")
+
---
+++ demo5.py
@@ -0,0 +1,12 @@
+for i in range(10):
+ if i == 5:
+  break
+ print(i)
+# Output: 0 1 2 3 4
+
+# Continue - skip current iteration
+for i in range(5):
+ if i == 4:
+  continue
+ print(i)
+# Output: 0 1 3 4
